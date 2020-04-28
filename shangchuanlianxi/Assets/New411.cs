e5d6a9d2ac8eebeefe3a559a8a132a0688420f22
hanyipengv <hanyipengv@hotmail.com>
2020-04-28 20:39:18 +0800

    更换安卓平台

--- shangchuanlianxi/Assets/New411.cs
+++ shangchuanlianxi/Assets/New411.cs
@@ -7,7 +7,7 @@ public class New411 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("asdssss");
+        Debug.Log("asdsasdasss");
     }
 
     // Update is called once per frame
